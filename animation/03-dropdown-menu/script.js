084508568a1e3e0037ad8e1f80d0f59158d5785a
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2024-06-28 14:56:26 -0700

    animation exercises

--- animation/03-dropdown-menu/script.js
+++ animation/03-dropdown-menu/script.js
@@ -2,14 +2,20 @@ const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
+window.setTimeout(function() {
+  dropdownMenu.classList.remove('hidden');
+}, 500);
+
 menuTitle.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
     dropdownMenu.classList.toggle("visible");
+    dropdownMenu.classList.toggle("hide");
   }  
 })
 
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("visible")
+    dropdownMenu.classList.toggle("visible")
+    dropdownMenu.classList.toggle("hide");
   }
 })
